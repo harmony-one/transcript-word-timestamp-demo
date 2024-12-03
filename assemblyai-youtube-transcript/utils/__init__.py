b3b8bb7a4170e05239267e807797dac5598c6cdd
fegloff <fegloff@gmail.com>
2024-12-03 18:25:31 -0500

    fix process_video_with_highlights (need to remove debug code comments)

--- assemblyai-youtube-transcript/utils/__init__.py
+++ assemblyai-youtube-transcript/utils/__init__.py
@@ -1,12 +1,13 @@
 from .cli import parse_arguments
 from .logging_config import setup_logger
 from .time_utils import format_time, millisec_to_srt_time
-from .text_utils import get_segment_texts
+from .text_utils import get_segment_texts, get_ass_style
 
 __all__ = [
     'parse_arguments',
     'setup_logger',
     'format_time',
     'millisec_to_srt_time',
-    'get_segment_texts'
+    'get_segment_texts',
+    'get_ass_style'
 ]
--- assemblyai-youtube-transcript/utils/text_utils.py
+++ assemblyai-youtube-transcript/utils/text_utils.py
@@ -14,3 +14,20 @@ def get_segment_texts(full_text: str) -> Tuple[str, str]:
           end_text = ' '.join(words[-5:])
           
       return start_text, end_text
+
+
+def get_ass_style(font_size: int = 120, margin_v: int = 250) -> str:
+    """Returns ASS subtitle configuration with customizable styling."""
+    return f"""[Script Info]
+ScriptType: v4.00+
+PlayResX: 1920
+PlayResY: 1080
+WrapStyle: 0
+
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: Default,Arial,{font_size},&HFFFFFF&,&H000000&,&H000000&,&H00000000,1,0,0,0,100,100,0,0,1,2,0,2,20,20,{margin_v},1
+
+[Events]
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+"""
